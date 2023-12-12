1b1d213e7d3c03d09f85bfc734389ff6e2029336
chalscc <carlescatalancalabuig@gmail.com>
2023-12-12 11:08:35 +0100

    Estructura pages

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      
+      {/* Login y registro */}
+      <Route />
+
+      {/* Journal App */}
+      <Route />
+
+    </Router>
+  )
+}
